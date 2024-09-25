27c6468732dd0e76abff5b8ffef400468b5bba81
siiraj0 <sirajkp752@gmail.com>
2024-09-25 10:51:25 +0530

    updated

--- sorting.js
+++ sorting.js
@@ -318,3 +318,53 @@
 
 
 
+// function mergeSort(arr){
+//     if(arr.length < 2){
+//         return arr
+//     }
+//     let mid = Math.floor(arr.length / 2)
+//     let left = arr.slice(0,mid)
+//     let right = arr.slice(mid)
+//     return merge(mergeSort(left), mergeSort(right))
+// }
+
+// function merge(left , right){
+// let sortedArr = []
+//   while(left.length && right.length){
+//     if(left[0] <= right[0]){
+//         sortedArr.push(left.shift())
+//     }else{
+//         sortedArr.push(right.shift())
+//     }
+//   }
+//   return [...sortedArr,...left,...right]
+// }
+
+// let arr= [4,7,-1,2,4,3]
+
+// console.log(mergeSort(arr));
+
+
+// function selectionSort(arr){
+//     for(let i = 0 ; i < arr.length - 1 ; i++){
+//         let minIndex = i
+//         for(let j = i+1 ; j < arr.length ; j++){
+//             if(arr[j] < arr[minIndex]){
+//                 minIndex = j
+//             }
+//         }
+//         if(minIndex !== i){
+//             let temp = arr[i]
+//             arr[i] = arr[minIndex]
+//             arr[minIndex] = temp
+//         }
+//     }
+//     return arr
+// }
+
+// let arr = [2,6,4,-1,5,8,7]
+// console.log(selectionSort(arr));
+
+
+
+
