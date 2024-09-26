d3950dd0a1b4f7a86b8837e96534d6aabb5a5345
siiraj0 <sirajkp752@gmail.com>
2024-09-26 16:39:44 +0530

    updated

---
+++ queue.js
@@ -0,0 +1,85 @@
+// class Queue{
+//     constructor(){
+//         this.items = []
+//     }
+
+//     enqueue(element){
+//         this.items.push(element)
+//     }
+//     dequeue(){
+//         return this.items.shift()
+//     }
+//     isEmpty(){
+//         return this.items.length === 0
+//     }
+//     peek(){
+//         if(!this.isEmpty()){
+//             return this.items[0]
+//         }
+//         return null
+//     }
+//     size(){
+//         return this.items.length
+//     }
+//     print(){
+//         console.log(this.items.toString());
+        
+//     }
+// }
+
+
+// const queue = new Queue()
+
+// console.log(queue.isEmpty());
+
+
+// queue.enqueue(10)
+// queue.enqueue(20)
+// queue.enqueue(30)
+// console.log(queue.peek());
+
+
+// console.log(queue.size());
+// queue.print()
+
+
+
+// class Queue{
+//     constructor(){
+//         this.items=[]
+//     }
+//     enque(element){
+//         this.items.push(element)
+//     }
+//     deque(){
+//         this.items.shift()
+//     }
+//     isEmpty(){
+//         return this.items.length === 0
+//     }
+//     peek(){
+//         if(!this.isEmpty()){
+//             return this.items[0]
+//         }
+//         return null
+//     }
+//     size(){
+//         return this.items.length
+//     }
+//     print(){
+//         console.log(this.items.toString());
+        
+//     }
+// }
+
+// const queue = new Queue()
+
+// console.log(queue.isEmpty());
+
+// queue.enque(10)
+// queue.enque(20)
+// queue.enque(30)
+
+
+// console.log(queue.size());
+// queue.print()
