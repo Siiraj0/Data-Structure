07136f31797f71f4711194cd3bb959c9c713c2ea
siiraj0 <sirajkp752@gmail.com>
2024-10-16 14:57:52 +0530

    updated

--- heap.js
+++ heap.js
@@ -177,75 +177,78 @@
 // console.log(hp)
 
 
-class Heap {
-    constructor() {
-        this.heap = [];
-    }
+// class Heap {
+//     constructor() {
+//         this.heap = [];
+//     }
 
-    insert(value) {
-        this.heap.push(value);
-        this.heapifyUp();
-    }
+//     insert(value) {
+//         this.heap.push(value);
+//         this.heapifyUp();
+//     }
 
-    heapifyUp() {
-        let index = this.heap.length - 1;
-        let parent = Math.floor((index - 1) / 2);
+//     heapifyUp() {
+//         let index = this.heap.length - 1;
+//         let parent = Math.floor((index - 1) / 2);
 
-        while (index > 0 && this.heap[index] > this.heap[parent]) {
-            [this.heap[index], this.heap[parent]] = [this.heap[parent], this.heap[index]];
+//         while (index > 0 && this.heap[index] > this.heap[parent]) {
+//             [this.heap[index], this.heap[parent]] = [this.heap[parent], this.heap[index]];
 
-            index = parent;
-            parent = Math.floor((index - 1) / 2);
-        }
-    }
+//             index = parent;
+//             parent = Math.floor((index - 1) / 2);
+//         }
+//     }
 
-    remove() {
-        if (this.heap.length === 0) return null;
+//     remove() {
+//         if (this.heap.length === 0) return null;
 
-        let max = this.heap[0];
-        this.heap[0] = this.heap.pop(); // Replace root with the last element
-        this.heapifyDown(); // Restore heap property by heapifying down
-        return max; // Return the removed max value
-    }
+//         let max = this.heap[0];
+//         this.heap[0] = this.heap.pop(); // Replace root with the last element
+//         this.heapifyDown(); // Restore heap property by heapifying down
+//         return max; // Return the removed max value
+//     }
 
-    heapifyDown() {
-        let index = 0;
-        let length = this.heap.length;
+//     heapifyDown() {
+//         let index = 0;
+//         let length = this.heap.length;
 
-        while (this.toGetLeft(index) < length) {
-            let large = this.toGetLeft(index);
-            let right = this.toGetRight(index);
+//         while (this.toGetLeft(index) < length) {
+//             let large = this.toGetLeft(index);
+//             let right = this.toGetRight(index);
 
-            if (right < length && this.heap[right] > this.heap[large]) {
-                large = right; // Choose the larger child
-            }
+//             if (right < length && this.heap[right] > this.heap[large]) {
+//                 large = right; // Choose the larger child
+//             }
 
-            if (this.heap[index] >= this.heap[large]) break; // Heap property satisfied
+//             if (this.heap[index] >= this.heap[large]) break; // Heap property satisfied
 
-            [this.heap[index], this.heap[large]] = [this.heap[large], this.heap[index]];
-            index = large;
-        }
-    }
+//             [this.heap[index], this.heap[large]] = [this.heap[large], this.heap[index]];
+//             index = large;
+//         }
+//     }
 
-    toGetLeft(index) {
-        return (2 * index) + 1;
-    }
+//     toGetLeft(index) {
+//         return (2 * index) + 1;
+//     }
 
-    toGetRight(index) {
-        return (2 * index) + 2;
-    }
-}
+//     toGetRight(index) {
+//         return (2 * index) + 2;
+//     }
+// }
 
-// Example usage:
-let hp = new Heap();
-hp.insert(30);
-hp.insert(20);
-hp.insert(10);
-hp.insert(50);
-hp.insert(70);
+// // Example usage:
+// let hp = new Heap();
+// hp.insert(30);
+// hp.insert(20);
+// hp.insert(10);
+// hp.insert(50);
+// hp.insert(70);
 
-console.log(hp.heap); // [70, 50, 10, 20, 30]
+// console.log(hp.heap); // [70, 50, 10, 20, 30]
 
-hp.remove(); // Removes 70
+// hp.remove(); // Removes 70
 
-console.log(hp.heap); // [50, 30, 10, 20]
+// console.log(hp.heap); // [50, 30, 10, 20]
+
+
+
